9fbef81b519f7bf263f5135f89aa9a7b019ccab5
Игорь Ткаченко <tegor1746@gmail.com>
2021-07-10 01:08:09 +0300

    Добавьте файлы проекта.

---
+++ PrimeNumberSearch/PrimeFactorDetector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumberSearch
+{
+    public static class PrimeFactorDetector
+    {
+        public static int getMaxPrime(int[] arrayInt)
+        {
+            int maxValue = 0;
+            int maxPrimeNumbers = 0;
+
+            foreach (var item in arrayInt)
+            {
+                if (!isPrime(item))
+                {
+                    int temp = getFactorization(item).Count;
+                    if (maxValue <= item && maxPrimeNumbers <= temp)
+                    {
+                        maxPrimeNumbers = temp;
+                        maxValue = item;
+                    }
+                }
+            }
+
+            return maxValue;
+        }
+
+        private static bool isPrime(int value)
+        {
+            if (value <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < Math.Sqrt(value); i++)
+            {
+                if (value % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static List<int> getFactorization(int value)
+        {
+            List<int> vs = new List<int>();
+            for (int i = 2; i <= value; i++)
+            {
+                if (isPrime(i))
+                {
+                    if (value % i == 0)
+                    {
+                        value /= i;
+                        vs.Add(i);
+                        i = 1;
+                    }
+                }
+            }
+            return vs;
+        }
+    }
+}
---
+++ PrimeNumberSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeNumberSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter an array of integers separated by a space.\nSample:1 2 3 5 10 11");
+                int[] arrayInt;
+                string[] inputLine = Console.ReadLine().Trim().Split(' ');
+                arrayInt = new int[inputLine.Length];
+                for (int i = 0; i < arrayInt.Length; i++)
+                {
+                    arrayInt[i] = int.Parse(inputLine[i]);
+                }
+
+                Console.WriteLine(PrimeFactorDetector.getMaxPrime(arrayInt));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error!\nThe input string was not formatted correctly!");
+            }
+
+        }
+    }
+}
